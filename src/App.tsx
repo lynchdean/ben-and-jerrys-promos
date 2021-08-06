40ec5bbd47c5dcc144f790ea9ecd09d70a6892af
Dean <lynchdean@gmail.com>
2021-08-06 19:08:12 +0100

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#" tab-index="-1" aria-disabled="true">Disabled</a>
-        </li>
-    </ul>
-  );
-}
+    <h1>Test</h1>
+  )
+  }
 
 export default App;
