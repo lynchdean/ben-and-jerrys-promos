821d281848c20e87b49a4fe3a21b4182127b9fe9
Dean <lynchdean@gmail.com>
2021-08-06 19:31:30 +0100

    Rick roll

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <h1>Test</h1>
-  )
-  }
+    <div>
+      <p>We're no strangers to love</p>
+      <p>You know the rules and so do I</p>
+      <p>A full commitment's what I'm thinking of</p>
+      <p>You wouldn't get this from any other guy</p>
+      <p>I just wanna tell you how I'm feeling</p>
+      <p>Gotta make you understand</p>
+      <p>Never gonna give you up</p>
+      <p>Never gonna let you down</p>
+      <p>Never gonna run around and desert you</p>
+      <p>Never gonna make you cry</p>
+      <p>Never gonna say goodbye</p>
+      <p>Never gonna tell a lie and hurt you</p>
+      <p>We've known each other for so long</p>
+      <p>Your heart's been aching but you're too shy to say it</p>
+      <p>Inside we both know what's been going on</p>
+      <p>We know the game and we're gonna play it</p>
+      <p>And if you ask me how I'm feeling</p>
+      <p>Don't tell me you're too blind to see</p>
+      <p>Never gonna give you up</p>
+      <p>Never gonna let you down</p>
+      <p>Never gonna run around and desert you</p>
+      <p>Never gonna make you cry</p>
+      <p>Never gonna say goodbye</p>
+      <p>Never gonna tell a lie and hurt you</p>
+      <p>Never gonna give you up</p>
+      <p>Never gonna let you down</p>
+      <p>Never gonna run around and desert you</p>
+      <p>Never gonna make you cry</p>
+      <p>Never gonna say goodbye</p>
+      <p>Never gonna tell a lie and hurt you</p>
+      <p>Never gonna give, never gonna give</p>
+      <p>(Give you up)</p>
+      <p>We've known each other for so long</p>
+      <p>Your heart's been aching but you're too shy to say it</p>
+      <p>Inside we both know what's been going on</p>
+      <p>We know the game and we're gonna play it</p>
+      <p>I just wanna tell you how I'm feeling</p>
+      <p>Gotta make you understand</p>
+      <p>Never gonna give you up</p>
+      <p>Never gonna let you down</p>
+      <p>Never gonna run around and desert you</p>
+      <p>Never gonna make you cry</p>
+      <p>Never gonna say goodbye</p>
+      <p>Never gonna tell a lie and hurt you</p>
+      <p>Never gonna give you up</p>
+      <p>Never gonna let you down</p>
+      <p>Never gonna run around and desert you</p>
+      <p>Never gonna make you cry</p>
+      <p>Never gonna say goodbye</p>
+      <p>Never gonna tell a lie and hurt you</p>
+      <p>Never gonna give you up</p>
+      <p>Never gonna let you down</p>
+      <p>Never gonna run around and desert you</p>
+      <p>Never gonna make you cry</p>
+      <p>Never gonna say goodbye</p>
+    </div>
+  );
+}
 
 export default App;
